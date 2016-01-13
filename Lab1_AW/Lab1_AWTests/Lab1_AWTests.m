54f0ff4f6c43c82e67d8515f95aa00fd5211df85
anthonywittemann <awittema@usc.edu>
2016-01-13 13:14:44 -0800

    Lab 1
    Created Lab 1 project

---
+++ Lab1_AW/Lab1_AWTests/Lab1_AWTests.m
@@ -0,0 +1,39 @@
+//
+//  Lab1_AWTests.m
+//  Lab1_AWTests
+//
+//  Created by Anthony Wittemann on 1/13/16.
+//  Copyright © 2016 Anthony Wittemann. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Lab1_AWTests : XCTestCase
+
+@end
+
+@implementation Lab1_AWTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
