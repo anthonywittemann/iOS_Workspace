6918441290ef6f1ec4795ed0cf8a2be6be30884e
anthonywittemann <awittema@usc.edu>
2016-01-25 12:19:58 -0800

    Starting on Lab 2

---
+++ Lab2_AW/Lab2_AW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab2_AW
+//
+//  Created by Anthony Wittemann on 1/25/16.
+//  Copyright © 2016 Anthony Wittemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lab2_AW/Lab2_AWTests/Lab2_AWTests.m
@@ -0,0 +1,39 @@
+//
+//  Lab2_AWTests.m
+//  Lab2_AWTests
+//
+//  Created by Anthony Wittemann on 1/25/16.
+//  Copyright © 2016 Anthony Wittemann. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Lab2_AWTests : XCTestCase
+
+@end
+
+@implementation Lab2_AWTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
