eeb59fb57313cbffe06b3fb212335f91acd7fa60
anthonywittemann <awittema@usc.edu>
2016-02-08 13:07:28 -0800

    Week 5 MVC Work

---
+++ week5/week5/Week5ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  week5
+//
+//  Created by Anthony Wittemann on 2/8/16.
+//  Copyright © 2016 Anthony Wittemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Week5ViewController : UIViewController
+
+
+@end
+
---
+++ week5/week5/Week5ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  week5
+//
+//  Created by Anthony Wittemann on 2/8/16.
+//  Copyright © 2016 Anthony Wittemann. All rights reserved.
+//
+
+#import "Week5ViewController.h"
+
+@interface Week5ViewController ()
+
+@end
+
+@implementation Week5ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
