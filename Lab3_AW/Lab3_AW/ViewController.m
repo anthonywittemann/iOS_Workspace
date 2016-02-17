88a3c125f1e9c0ae1e9658777076a5f8cebe44c4
anthonywittemann <awittema@usc.edu>
2016-02-17 12:17:27 -0800

    adding ibActions

--- Lab3_AW/Lab3_AW/ViewController.m
+++ Lab3_AW/Lab3_AW/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billTextField;
 
 @end
 
@@ -23,5 +24,37 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(IBAction)dismissKeyboard:(id)sender{
+    [sender resignFirstResponder];
+}
+- (IBAction)segmentedControlTax:(id)sender {
+    //TODO
+}
+
+- (IBAction)textFieldExit:(id)sender {
+    [sender resignFirstResponder];
+}
+- (IBAction)switchFlippedTipIncludesTax:(id)sender {
+    //TODO
+}
+- (IBAction)sliderModifiedTip:(id)sender {
+}
+
+- (void) touchesBegan: (NSSet *)touches
+            withEvent:(UIEvent *)event {
+    UITouch *touch = [[event allTouches] anyObject];
+    if ([self.billTextField isFirstResponder] &&
+        [touch view] != self.billTextField) {
+        [self.billTextField resignFirstResponder];
+    }
+    [super touchesBegan:touches withEvent:event];
+}
+
+- (IBAction) backgroundTouched:(id)sender {
+    [self.billTextField resignFirstResponder];
+}
+
+
 
 @end
