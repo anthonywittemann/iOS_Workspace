b8d13822204ea1a217062401ae1b30a679619fd0
anthonywittemann <awittema@usc.edu>
2016-04-27 11:08:53 -0700

    starting on lab 6 ec

---
+++ Lab6-EC/Lab6-EC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab6-EC
+//
+//  Created by Anthony Wittemann on 4/27/16.
+//  Copyright © 2016 Anthony Wittemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lab6-EC/Lab6-EC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab6-EC
+//
+//  Created by Anthony Wittemann on 4/27/16.
+//  Copyright © 2016 Anthony Wittemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
