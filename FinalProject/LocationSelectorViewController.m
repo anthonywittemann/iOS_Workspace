f82792dc8c575308a4c5a6cc95b1ac06523a2c48
anthonywittemann <awittema@usc.edu>
2016-05-03 15:47:41 -0700

    still working to get location to work

--- FinalProject/LocationSelectorViewController.m
+++ FinalProject/LocationSelectorViewController.m
@@ -15,10 +15,14 @@
 //needed for google maps API
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+@property (weak, nonatomic) IBOutlet UILabel *currentNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *currentAddressLabel;
 
 //for UI not on google maps
 @property (weak, nonatomic) IBOutlet UILabel *locationNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *locationAddressLabel;
+@property (weak, nonatomic) IBOutlet UILabel *currentLocNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *currentLocAddressLabel;
 
 
 @property (strong,nonatomic) TransactionModel *model;
@@ -36,22 +40,6 @@ GMSPlacePicker *_placePicker;
     
     self.model = [TransactionModel sharedModel];
     // Do any additional setup after loading the view.
-//    
-//    // Create a GMSCameraPosition that tells the map to display the
-//    // coordinate -33.86,151.20 at zoom level 6.
-//    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
-//                                                            longitude:151.20
-//                                                                 zoom:6];
-//    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
-//    mapView_.myLocationEnabled = YES;
-//    self.view = mapView_;
-//    
-//    // Creates a marker in the center of the map.
-//    GMSMarker *marker = [[GMSMarker alloc] init];
-//    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
-//    marker.title = @"Sydney";
-//    marker.snippet = @"Australia";
-//    marker.map = mapView_;
     
     _placesClient = [[GMSPlacesClient alloc] init];
     self.locationNameLabel.text = self.model.getLocationName;
@@ -63,7 +51,7 @@ GMSPlacePicker *_placePicker;
     // Dispose of any resources that can be recreated.
 }
 
-/* - to get current location
+// to get current location - TODO NOT WORKING
 // Add a UIButton in Interface Builder to call this function
 - (IBAction)getCurrentPlace:(UIButton *)sender {
     [_placesClient currentPlaceWithCallback:^(GMSPlaceLikelihoodList *placeLikelihoodList, NSError *error){
@@ -72,25 +60,28 @@ GMSPlacePicker *_placePicker;
             return;
         }
         
-        self.nameLabel.text = @"No current place";
-        self.addressLabel.text = @"";
+        self.currentNameLabel.text = @"No current place";
+        self.currentAddressLabel.text = @"";
         
         if (placeLikelihoodList != nil) {
             GMSPlace *place = [[[placeLikelihoodList likelihoods] firstObject] place];
             if (place != nil) {
-                self.nameLabel.text = place.name;
-                self.addressLabel.text = [[place.formattedAddress componentsSeparatedByString:@", "]
+                self.currentNameLabel.text = place.name;
+                self.currentAddressLabel.text = [[place.formattedAddress componentsSeparatedByString:@", "]
                                           componentsJoinedByString:@"\n"];
             }
         }
+        [self.model setCurrentLocationName:self.currentNameLabel.text];
+        [self.model setCurrentLocationAddress:self.currentAddressLabel.text];
+        NSLog(@"^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v");
     }];
 }
-*/
+
 
 // Add a UIButton in Interface Builder to call this function
 - (IBAction)pickPlace:(UIButton *)sender {
     
-    CLLocationCoordinate2D center = CLLocationCoordinate2DMake(37.788204, -122.411937);
+    CLLocationCoordinate2D center = CLLocationCoordinate2DMake(34.0224, -118.2851);
     CLLocationCoordinate2D northEast = CLLocationCoordinate2DMake(center.latitude + 0.001,
                                                                   center.longitude + 0.001);
     CLLocationCoordinate2D southWest = CLLocationCoordinate2DMake(center.latitude - 0.001,
@@ -119,7 +110,7 @@ GMSPlacePicker *_placePicker;
         [self.model setLocationAddress:self.addressLabel.text];
         self.locationNameLabel.text = self.nameLabel.text;
         self.locationAddressLabel.text = self.addressLabel.text;
-        NSLog(@"GOOOOOT HEREEEEE!!!");
+        NSLog(@"GOOOOOT HEREEEEE!!!"); // TODO - data not being returned or displayed
     }];
 }
 
